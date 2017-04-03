fc5ba1fc63edf4ceedc3022b8b59e8f5ebeb6a22
Marcos Lilljedahl <marcosnils@gmail.com>
2017-04-03 16:29:24 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# sherloq-talk
-Sherloq Talk plugin
+# Sherloq Talk plugin
+
+The following plugin sends Talk (https://coralproject.net/products/talk.html) comments to Sherloq for moderation
+
+
+## Configuration
+
+
+Set the following env variables in your talk setup
+
+- `SHERLOQ_TOKEN` (required) - Sherloq account API token
+- `SHERLOQ_API_URL` (optional for dev setup) - Sherloq API endpoint 
---
+++ index.js
@@ -0,0 +1,31 @@
+const url = process.env.SHERLOQ_API_URL || 'https://api.sherloq.io';
+const token = process.env.SHERLOQ_TOKEN;
+const auth = require('basic-auth');
+const fetch = require('node-fetch');
+
+module.exports = {
+  hooks: {
+    RootMutation: {
+      createComment: {
+        post: async (obj, args) => {
+          let body = {
+            "data": {
+              "text": args.body
+            },
+            "criteria": ["hate_speech"]
+          };
+
+          let headers = {
+            "content-type": "application/json",
+            "authorization": "Basic " + new Buffer(token + ":").toString("base64")
+          }
+
+          return fetch(url + '/moderations', {method: 'POST', body: JSON.stringify(body), headers: headers})
+          .catch(function(err){
+            console.log(err);
+          });
+        }
+      }
+    }
+  }
+};
