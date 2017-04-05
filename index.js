3d5afa45002a7004567c9f2e20915e8417b6a0ef
Marcos Lilljedahl <marcosnils@gmail.com>
2017-04-05 17:17:21 -0300

    Update pluggin to it accepts moderations with low score

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const url = process.env.SHERLOQ_API_URL || 'https://api.sherloq.io';
 const token = process.env.SHERLOQ_TOKEN;
-const auth = require('basic-auth');
+const autoAction = process.env.SHERLOQ_AUTO_ACTION || false;
 const fetch = require('node-fetch');
+const CommentsService = require('services/comments.js')
 
 module.exports = {
   hooks: {
     RootMutation: {
       createComment: {
-        post: async (obj, args) => {
+        post: async (obj, args, context, info, result) => {
           let body = {
             "data": {
               "text": args.body
             },
             "criteria": ["hate_speech"],
             "labels": {
-              "comment_id": args.asset_id
+              "comment_id": result.comment.id,
+              "asset_id": result.asset_id
             }
           };
 
@@ -23,10 +25,20 @@ module.exports = {
             "authorization": "Basic " + new Buffer(token + ":").toString("base64")
           }
 
-          return fetch(url + '/moderations', {method: 'POST', body: JSON.stringify(body), headers: headers})
-          .catch(function(err){
+          await fetch(url + '/moderations', {method: 'POST', body: JSON.stringify(body), headers: headers})
+          .then(res => res.json())
+          .then(json => {
+            if (autoAction) {
+              if (json.scores.hate_speech[0].score < 90) {
+                return CommentsService.setStatus(result.comment.id, 'ACCEPTED')
+                .then(() => result.comment.status = 'ACCEPTED')
+              }
+            }
+          })
+          .catch(function(err) {
             console.log(err);
           });
+          return result;
         }
       }
     }
